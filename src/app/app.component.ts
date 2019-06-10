75d661c1114329e058986cf7758f7545a010a48f
rubyaice <andrex.silva.777@gmail.com>
2019-06-10 07:22:24 -0430

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularTest';
+
+  nombre = 'Andres Silva';
+  edad = 26;
+  email = 'andrex.silva.777@gmail.com';
+  sueldos = [1700, 1600, 1500];
+  activo = true;
+
+  // metood para saber si el empleado esta activo o no
+  esActivo() {
+
+    if (this.activo) {
+      return 'El empleado esta activo';
+    } else {
+      return 'El empleado esta inactivo';
+    }
+  }
+
+  // metodo para hacer la suma de los sueldos del empleado
+  ultimos3Sueldos() {
+
+    let suma = 0;
+
+// tslint:disable-next-line: prefer-for-of
+    for (let x = 0; x < this.sueldos.length; x++) {
+      suma = suma + this.sueldos[x];
+    }
+
+    return suma;
+  }
+}
